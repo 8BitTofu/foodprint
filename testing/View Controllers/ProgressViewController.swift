615ef54db1fef35b5aee280f33e1daaa7d417778
Daniel Mishkanian <39421814+dmishkan@users.noreply.github.com>
2021-03-10 19:29:28 -0800

    Fetched HealthApp Data

--- testing/View Controllers/ProgressViewController.swift
+++ testing/View Controllers/ProgressViewController.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
+import HealthKit
 
 class ProgressViewController: UIViewController {
 
     // MARK: Buttons / Labels
     
     @IBOutlet weak var stepometerButton: UIButton!
+    @IBOutlet weak var lblAge: UILabel!
     
+    
+    let healthKitStore:HKHealthStore = HKHealthStore()
     
     
     override func viewDidLoad() {
@@ -28,17 +32,61 @@ class ProgressViewController: UIViewController {
     }
     
     
+    @IBAction func authorizeKitClicked(_ sender: Any) {
+        self.authorizeHealthKitApp()
+    }
     
-    
+    @IBAction func getDetails(_ sender: Any)
+    {
+        let age = self.readProfile()
+        self.lblAge.text = "\(String(describing: age))"
+    }
 
     // MARK: Transitions
     
-    func transitionToStepometerView() {
+    func transitionToStepometerView(){
         // transition to account screen
         let stepometerViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.stepometerViewController) as? StepometerViewController
         
         view.window?.rootViewController = stepometerViewController
         view.window?.makeKeyAndVisible()
     }
+    
+    func readProfile() -> Int?
+    {
+        var age:Int?
+        
+        do {
+            let birthDay = try healthKitStore.dateOfBirthComponents()
+            let calendar = Calendar.current
+            let currentyear = calendar.component(.year, from: Date())
+            age = currentyear - birthDay.year!
+            
+        } catch {}
+        
 
+        return age
+    }
+    
+    func authorizeHealthKitApp()
+    {
+        let healthKitTypesToRead: Set<HKObjectType> = [
+            HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.dateOfBirth)!,
+            HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.bloodType)!,
+            //HKObjectType.quantityType(forIdentifier: .bodyMass)!,
+        ]
+        
+        let healthKitTypesToWrite : Set<HKSampleType> = []
+        
+        if !HKHealthStore.isHealthDataAvailable()
+        {
+            print("Error occured")
+            return
+        }
+        
+        healthKitStore.requestAuthorization(toShare: healthKitTypesToWrite, read: healthKitTypesToRead)
+        { (success, error) -> Void in
+            print("Read Write Authorization succeeded")
+        }
+    }
 }
--- testing/View Controllers/StepometerViewController.swift
+++ testing/View Controllers/StepometerViewController.swift
@@ -8,9 +8,10 @@
 import Foundation
 import CoreMotion
 import UIKit
+import HealthKit
 
 
-class StepometerViewController: UIViewController {
+class StepometerViewController: UIViewController{
     
     
     @IBOutlet weak var lbCounter: UILabel!
@@ -26,6 +27,8 @@ class StepometerViewController: UIViewController {
 
     let activityManager = CMMotionActivityManager()
     let pedometer = CMPedometer()
+    
+    let healthKitStore:HKHealthStore = HKHealthStore()
     
 
 
@@ -66,6 +69,37 @@ class StepometerViewController: UIViewController {
         
     @IBAction func backTapped(_ sender: Any) {
         transitionToHome()
+    }
+    
+    @IBAction func authorizeKitClicked(_ sender: Any) {
+        self.authorizeHealthKitApp()
+    }
+    
+    func getHealthInfo()
+    {
+        
+    }
+    
+    func authorizeHealthKitApp()
+    {
+        let healthKitTypesToRead: Set<HKObjectType> = [
+            HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.dateOfBirth)!,
+            HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.bloodType)!,
+            //HKObjectType.quantityType(forIdentifier: .bodyMass)!,
+        ]
+        
+        let healthKitTypesToWrite : Set<HKSampleType> = []
+        
+        if !HKHealthStore.isHealthDataAvailable()
+        {
+            print("Error occured")
+            return
+        }
+        
+        healthKitStore.requestAuthorization(toShare: healthKitTypesToWrite, read: healthKitTypesToRead)
+        { (success, error) -> Void in
+            print("Read Write Authorization succeeded")
+        }
     }
     
     
